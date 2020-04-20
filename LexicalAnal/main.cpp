2fbc8a6f574a62a41c990b896a8d9fc13cd3475d
uigohiiming2@naver.com <uigohiiming2@naver.com>
2020-04-20 23:39:26 +0900

    Initial Commit

---
+++ LexicalAnal/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LexicalAnal
+//
+//  Created by 양준식 on 2020/04/20.
+//  Copyright © 2020 양준식. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
